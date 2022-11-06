d7016d3f5d07d21498ed83913005c0f205c985e7
WayneTzeng <66355598+WayneTzeng@users.noreply.github.com>
2022-11-06 18:22:17 +0800

    部屬測試

--- lingua_terra_interface/vue.config.js
+++ lingua_terra_interface/vue.config.js
@@ -1,7 +1,8 @@
-// const { defineConfig } = require('@vue/cli-service')
-// module.exports = defineConfig({
-//   transpileDependencies: true
-// })
-module.exports = {
-  publicPath: '/lingua_terra_interface/'
-}
+const { defineConfig } = require('@vue/cli-service')
+module.exports = defineConfig({
+  transpileDependencies: true,
+  publicPath:'/'
+})
+// module.exports = {
+//   publicPath: '/lingua_terra_interface/'
+// }
